be05f3ca33a803fcceba1a55fb4fabaa82880851
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2023-05-23 07:50:33 -0300

    Seção 3: Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,98 +13,41 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-         public EventoController()
-         {
-             
+        
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
         }
 
-        public IEnumerable<Evento> _evento = new Evento[]{
-        
-                new Evento {
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Belo Horizonte",
-                    Lote = "1° Lote",
-                    QtdPessoas = 250,
-                    DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto.png"
-                },
-                 new Evento {
-                    EventoId = 2,
-                    Tema = "Angular e sua Novidades",
-                    Local = "São Paulo",
-                    Lote = "2° Lote",
-                    QtdPessoas = 350,
-                    DataEvento =  DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto1.png"
-                },
-                 new Evento {
-                    EventoId = 3,
-                    Tema = ".NET 5",
-                    Local = "Rio de Janeiro",
-                    Lote = "3° Lote",
-                    QtdPessoas = 150,
-                    DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto2.png"
-                }
-        };
-        
-
-         [HttpGet]
-        public IEnumerable<Evento> Get()
+        [HttpGet]
+        public IEnumerable<Evento> GetById()
         {
-           return new Evento[]{
-                new Evento {
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Belo Horizonte",
-                    Lote = "1° Lote",
-                    QtdPessoas = 250,
-                    DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto.png"
-                },
-                 new Evento {
-                    EventoId = 2,
-                    Tema = "Angular e sua Novidades",
-                    Local = "São Paulo",
-                    Lote = "2° Lote",
-                    QtdPessoas = 350,
-                    DataEvento =  DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto1.png"
-                },
-                 new Evento {
-                    EventoId = 3,
-                    Tema = ".NET 5",
-                    Local = "Rio de Janeiro",
-                    Lote = "3° Lote",
-                    QtdPessoas = 150,
-                    DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto2.png"
-                }
-           };
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id){
-            return _evento.Where(evento => evento.EventoId == id);
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
-         [HttpPost]
+        [HttpPost]
         public String Post()
         {
-           return "Exemplo de Post";
+            return "Exemplo de Post";
         }
 
-         [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public String Put(int id)
         {
-           return $"Exemplo de Put com id = {id}";
+            return $"Exemplo de Put com id = {id}";
         }
 
-         [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public String Delete(int id)
         {
-           return $"Exemplo de Delete com id = {id}";
+            return $"Exemplo de Delete com id = {id}";
         }
     }
 }
